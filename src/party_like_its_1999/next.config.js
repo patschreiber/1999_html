61bcfd13bfd3b22be715eaab2d45754281e7f33c
Patrick Schreiber <2431707+patschreiber@users.noreply.github.com>
2023-04-10 22:29:20 -0500

    NextJS (tabling for now (WIP))

--- src/party_like_its_1999/next.config.js
+++ src/party_like_its_1999/next.config.js
@@ -1,6 +1,36 @@
-/** @type {import('next').NextConfig} */
+const pkg = require('./package.json');
+
+// Apply HTTP response headers to all routes in the application.
+const securityHeaders = [
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  }
+];
+
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   reactStrictMode: true,
+  // specify a name to use for a custom build directory to use instead of .next.
+  distDir: './dist',
+
+  generateBuildId: async () => {
+    return pkg.version;
+  },
+
+
+  // Apply these headers to all routes in your application.
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
+
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
